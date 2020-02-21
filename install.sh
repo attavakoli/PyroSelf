fba70583c049906730a7d1496f437513b14d3bdb
Alireza Tavakoli <51003080+attavakoli@users.noreply.github.com>
2020-02-21 15:01:43 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt install python3-pip
+sudo python3.6 -m pip install -U setuptools
+sudo python3.6 -m pip install -U pyrogram
+sudo python3.6 -m pip install -U pyrogram[tgcrypto]
+sudo python3.6 -m pip install --upgrade tgcrypto
+sudo python3.6 -m pip install Khayyam
+sudo python3.6 -m pip install redis
